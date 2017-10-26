ba06ee06ea3b2b74134c250164e048a900c7a6cf
Cristian Huichaqueo <cristianhuichaqueo@gmail.com>
2017-10-26 19:10:06 -0300

    security with keycloak

---
+++ src/main/java/com/quick/demo/config/SecurityConfig.java
@@ -0,0 +1,88 @@
+package com.quick.demo.config;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
+{
+   /** 
+    * Registers the KeycloakAuthenticationProvider with the authentication manager.
+    */
+   @Autowired
+   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+      keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+      auth.authenticationProvider(keycloakAuthenticationProvider);
+   }
+   
+   @Bean
+   public KeycloakConfigResolver KeycloakConfigResolver() {
+      return new KeycloakSpringBootConfigResolver();
+   }
+   /**
+    * Defines the session authentication strategy.
+    */
+   @Bean
+   @Override
+   protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+      return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+   }
+   
+   @Autowired
+   public KeycloakClientRequestFactory keycloakClientRequestFactory;
+
+   @Bean
+   @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+   public KeycloakRestTemplate keycloakRestTemplate() {
+       return new KeycloakRestTemplate(keycloakClientRequestFactory);
+   }
+   
+   @Bean
+   public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+           KeycloakAuthenticationProcessingFilter filter) {
+       FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+       registrationBean.setEnabled(false);
+       return registrationBean;
+   }
+
+   @Bean
+   public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+           KeycloakPreAuthActionsFilter filter) {
+       FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+       registrationBean.setEnabled(false);
+       return registrationBean;
+   }
+
+   @Override
+   protected void configure(HttpSecurity http) throws Exception
+   {
+      super.configure(http);
+      http
+            .authorizeRequests()
+            .anyRequest().permitAll();
+   }
+}
